fcd3aa4edbef15da5fe1c4bea03d37014eac9bdb
Raphael <raphaelprix99@gmail.com>
2026-03-15 02:20:40 -0300

    fix: adicionar fileReplacements no angular.json para environment de produção

--- front/src/app/services/auth.ts
+++ front/src/app/services/auth.ts
@@ -1,9 +1,9 @@
-gitimport { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { Observable } from 'rxjs'; 
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
